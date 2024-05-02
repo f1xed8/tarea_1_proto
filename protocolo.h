381f0700fa48af754d543db0a11f56e1c52c6e89
Cristopher <74779158+CristopherApu@users.noreply.github.com>
2024-05-02 16:32:58 -0400

    Update protocolo.h
    update en clases

--- protocolo.h
+++ protocolo.h
@@ -1,21 +1,19 @@
 #ifndef TAREA_1_PROTOCOLO_H
 #define TAREA_1_PROTOCOLO_H
-//SE DEFINE EL TIPO BYTE
-#define BYTE unsigned char
-
-//SE DEFINEN LOS PINES
-
-//#define VELOCIDAD 500
-//#define PIN_RX 18
-
-
+#define BYTE unsigned char // Se define el tipo de byte
+#define LARGO_DATO 31
+#define LARGO_FRAME 35
+/*#define VELOCIDAD 500 // Se define la velocidad
+#define PIN_RX 18 // Se define el pin de recepción de señal
+#define PIN_RZ 17 // Se define el pin de envío de señal*/
 
 //  Se define la estructura del grupo 6 según el protocolo ya revisado por el profesor
 struct grupo6{
-    BYTE cmd;   // 4 -> 0100
-    BYTE lng;   // 5 -> 0101
-    BYTE data[4];   // 31 -> 1111 1111 1111 1111
-    BYTE fcs;   // 9 -> 1001
+    BYTE cmd;   // 4 bits
+    BYTE lng;   // 5 bits
+    BYTE data[LARGO_DATO];   // 31 bytes
+    BYTE fcs;   // 9 bits
+    BYTE frame[LARGO_FRAME];
 };
 
 #endif //TAREA_1_PROTOCOLO_H
