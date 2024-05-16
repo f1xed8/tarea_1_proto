c6788f98c9d3b54b1497356e95de2c66ef860cad
CristopherApu <cristopher.almonacid22@gmail.com>
2024-05-15 22:40:23 -0400

    intento de que no se cierre el receptor

--- receptor.cpp
+++ receptor.cpp
@@ -3,21 +3,24 @@
 
 int main(){
     grupo6 proto;
-    desempaquetamiento(proto, proto.lng);
-    if (proto.cmd == 1)
+    while (true)
     {
-        cerrar_receptor();
-    } else if (proto.cmd == 2){
-        int c = 0; int ed = 0; int end = 0;
-        for (size_t i = 1; i < 10; i++)
-        {
-            bool estado = desempaquetamiento(proto, proto.lng);
-            if (estado == true){
-                c = c++;
-            } else {
-                end = end++;
-                }          
-        }
+        recibir(proto);
+        bool estado = desempaquetamiento(proto, proto.lng);
+        if (proto.cmd == 1)
+            {
+                cerrar_receptor();
+            } else if (proto.cmd == 2){
+                int c = 0; int ed = 0; int end = 0;
+                for (size_t i = 1; i < 10; i++)
+                {
+                    bool estado = desempaquetamiento(proto, proto.lng);
+                    if (estado == true){
+                        c = c++;
+                        } else {
+                            end = end++;
+                        }          
+                }    
         float porcentajec = c/(c + ed + end);
         float porcentajeed = ed/(c + ed + end);
         float porcentajeend = end/(c + ed + end);
@@ -26,12 +29,13 @@ int main(){
         printf("%d mensajes recibidos con error no detectado\n", end);
         printf("Entonces tenemos una recepción del %%%f\n", porcentajec);
         printf("Entonces tenemos un porcentaje de error detectado de %%%f\n y un %%%f no detectado", porcentajeed, porcentajeend);
-    } else if (proto.cmd == 3){
-        recibir(proto);
-        // guardar en un "mensaje.txt"
-    } else if (proto.cmd == 4){
-        // mostrar "mensaje.txt" o indicar que no existe el archivo
-    }
+            } else if (proto.cmd == 3){
+                recibir(proto);
+                // guardar en un "mensaje.txt"
+                } else if (proto.cmd == 4){
+                    // mostrar "mensaje.txt" o indicar que no existe el archivo
+                }
+}
     
     return 0;
 }
