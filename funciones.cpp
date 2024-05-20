be4faa0e2022e6a730e47c32f55003b3b2539129
CristopherApu <cristopher.almonacid22@gmail.com>
2024-05-19 22:55:18 -0400

    11vo intento

--- funciones.cpp
+++ funciones.cpp
@@ -64,19 +64,10 @@ void recibir(grupo6 &proto){
     void limpiar_buffer();
     printf("inicia void recibir");
     bool estado = desempaquetamiento(proto, proto.lng); // Definimos la variable estado para almacenar el retorno de la función desempaquetamiento
-    printf("Se recibió un mensaje de manera %s\n",estado?"incorrecta":"correcta");
+    /*printf("Se recibió un mensaje de manera %s\n",estado?"incorrecta":"correcta");
     printf("El largo del mensaje es de %d bytes\n¿Desea visualizar el mensaje? (S/N): ", proto.lng);
     char SN;
-        while (true) {
-        // Leer entrada con fgets y luego analizar el primer carácter
-        char input[3]; // Suficiente para un carácter y el salto de línea
-        fgets(input, sizeof(input), stdin);
-
-        if (sscanf(input, "%c", &SN) == 1) {
-            break;
-        }
-        printf("Entrada inválida. Intente de nuevo: ");
-    }
+    scanf("%c", &SN);
     switch (SN) {
         case 'S':
         case 's':
@@ -92,7 +83,7 @@ void recibir(grupo6 &proto){
         default:
             printf("Opción inválida.\n");
             break;
-    }
+    }*/
 }
 void archivo_texto(){
 }
